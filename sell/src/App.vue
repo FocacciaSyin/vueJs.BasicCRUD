55640293ceb7d14c03fc62b7181627b0b6007e7e
FocacciaSyin <simoncat524@hotmail.com>
2021-01-13 23:49:46 +0800

    first Commit

---
+++ sell/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <test></test>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Test from "./components/Test.vue";
+import test from "./components/Test.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    Test,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ sell/src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">
+    我是測試組件
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Woody made.",
+    };
+  },
+};
+</script>
+
+<style>
+.test {
+  color: red;
+}
+</style>
